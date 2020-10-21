f9f7ffd918e8aca99d5cc1d0235e0f5ee51497b9
Saladsnek <505188@chrome.escg.ac.uk>
2020-10-21 16:07:19 +0100

    Coded movement and sorted issues regarding player catching on tiles

---
+++ HorrorGameProject/Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public int speedRun = 5;
+    public int speedJump = 1;
+    public bool facingRight = false;
+    public float moveOnX;
+    public bool grounded; 
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerMovement();
+    }
+
+    void playerMovement()
+    {
+
+        moveOnX = Input.GetAxis("Horizontal");
+        if (Input.GetButtonDown("Jump") && grounded == true)
+        {
+
+            Jump();
+        }
+        if (moveOnX < 0.0f && facingRight == false)
+        {
+            FlipPlayer();
+        }
+        else if (moveOnX > 0.0f && facingRight == true)
+        {
+            FlipPlayer();
+        }
+        gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(moveOnX * speedRun, gameObject.GetComponent<Rigidbody2D>().velocity.y);
+    }
+    void FlipPlayer()
+    {
+        facingRight = !facingRight;
+        Vector2 localScale = gameObject.transform.localScale;
+        localScale.x *= -1;
+        transform.localScale = localScale;
+    }
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "ground")
+        {
+            grounded = true;
+        }
+    }
+    void Jump()
+    {
+        GetComponent<Rigidbody2D>().AddForce(Vector2.up * speedJump);
+        grounded = false;
+    }
+}
